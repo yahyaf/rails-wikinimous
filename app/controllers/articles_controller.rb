1570ba6afd42d35e619b2a3e452c1533f0545e00
Yahya Fallah <yahya.fallah.araghi@gmail.com>
2019-02-12 18:37:40 +0100

    s6d2e3 in progress

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,41 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(task_params)
+    @article.save
+    redirect_to articles_path
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(task_params)
+    redirect_to articles_path
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def task_params
+    params.require(:article).permit(:title, :content)
+  end
+end
